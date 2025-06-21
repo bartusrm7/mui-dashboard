f3608a6b24559bf42a56549660c521a80b51a189
Bartek <bbogucki7@gmail.com>
2025-06-21 09:39:29 +0200

    create statistics components for display curious informations abouts cars

--- material-dashboard-backend/components/gpsAPI/vehiclesApi.js
+++ material-dashboard-backend/components/gpsAPI/vehiclesApi.js
@@ -59,14 +59,45 @@ router.get("/vehicle-localization", async (req, res) => {
 
 router.get("/vehicle-date-localization", async (req, res) => {
 	try {
-		const { registration, date } = req.query;
-		if (!registration || !date) return res.status(400).json({ error: "Registration number and date are required" });
+		const { registration, start_timestamp, end_timestamp } = req.query;
+		if (!registration || !start_timestamp || !end_timestamp)
+			return res.status(400).json({ error: "Registration number and date are required" });
 
 		const userNameGPSAuth = process.env.API_USER_NAME;
 		const userPasswordGPSAuth = process.env.API_USER_PASSWORD;
 		const authHeader = Buffer.from(`${userNameGPSAuth}:${userPasswordGPSAuth}`).toString("base64");
 
-		const response = await fetch(`https://fleetapi-pl.cartrack.com/rest/vehicles/${registration}/status`, {
+		const response = await fetch(
+			`https://fleetapi-pl.cartrack.com/rest/vehicles/${registration}/events?start_timestamp=${startDate}&end_timestamp=${endDate}`,
+			{
+				method: "GET",
+				headers: {
+					Authorization: `Basic ${authHeader}`,
+					"Content-type": "application/json",
+				},
+			}
+		);
+		if (!response.ok) {
+			const errorText = await response.text();
+			console.error(`API error ${response.status}: ${errorText}`);
+			throw new Error(`API error ${response.status}`);
+		}
+		const data = await response.json();
+		console.log(data.data[0]);
+		return res.status(200).json(data.data[0]);
+	} catch (error) {
+		console.error("Error in vehicle localization by date in api route", error);
+		return res.status(500).json({ error: "Server error" });
+	}
+});
+
+router.get("/vehicles-curious-data", async (req, res) => {
+	try {
+		const userNameGPSAuth = process.env.API_USER_NAME;
+		const userPasswordGPSAuth = process.env.API_USER_PASSWORD;
+		const authHeader = Buffer.from(`${userNameGPSAuth}:${userPasswordGPSAuth}`).toString("base64");
+
+		const response = await fetch("https://fleetapi-pl.cartrack.com/rest/vehicles/status", {
 			method: "GET",
 			headers: {
 				Authorization: `Basic ${authHeader}`,
@@ -79,9 +110,14 @@ router.get("/vehicle-date-localization", async (req, res) => {
 			throw new Error(`API error ${response.status}`);
 		}
 		const data = await response.json();
-		return res.status(200).json(data.data.location);
+		const vehiclesCuriousData = data.data.map(vehicleData => ({
+			registration: vehicleData.registration,
+			odometer: vehicleData.odometer,
+			road_speed: vehicleData.road_speed,
+		}));
+		return res.status(200).json(vehiclesCuriousData);
 	} catch (error) {
-		console.error("Error in gps vehicle localization api route", error);
+		console.error("Error in gps curious vehicles data api route", error);
 		return res.status(500).json({ error: "Server error" });
 	}
 });
