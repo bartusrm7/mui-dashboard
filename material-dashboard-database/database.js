8ae509934c4603f42e8f7c577041720497738f98
Bartek <bbogucki7@gmail.com>
2025-06-25 11:11:49 +0200

    fix env in database

--- material-dashboard-database/database.js
+++ material-dashboard-database/database.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
-	port: process.env.PORT,
+	host: process.env.DB_HOST || "localhost",
+	user: process.env.DB_USER || "root",
+	password: process.env.DB_PASSWORD || "",
+	database: process.env.DB_NAME || "mui_dashboard",
+	port: process.env.DB_PORT || 3306,
 });
 
 db.query(`CREATE TABLE IF NOT EXISTS userData (
