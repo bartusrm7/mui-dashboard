5d372d0b6154ca84937f67cf873f6fa1b0bea3ce
Bartek <bbogucki7@gmail.com>
2025-06-25 11:22:07 +0200

    test commit

--- material-dashboard-database/database.js
+++ material-dashboard-database/database.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
 	host: process.env.DB_HOST || "localhost",
 	user: process.env.DB_USER || "root",
 	password: process.env.DB_PASSWORD || "",
-	database: process.env.DB_NAME || "mui_dashboard",
+	database: process.env.DB_NAME,
 	port: process.env.DB_PORT || 3306,
 });
 
