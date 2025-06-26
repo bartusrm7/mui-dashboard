fc1d02533ebd7883d4580ce29f1bb6698154c400
Bartek <bbogucki7@gmail.com>
2025-06-26 10:59:13 +0200

    fix authorization function

--- material-dashboard-backend/components/auth/sign-in/login.js
+++ material-dashboard-backend/components/auth/sign-in/login.js
@@ -7,7 +7,9 @@ const { loginValidation } = require("./loginValidation");
 const router = express.Router();
 
 function authenticateToken(req, res, next) {
-	const token = req.cookies.accessToken;
+	const authHeader = req.headers["authorization"];
+	const token = authHeader && authHeader.split(" ")[1];
+
 	if (!token) return res.status(401).json({ error: "Token is not provide" });
 
 	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
