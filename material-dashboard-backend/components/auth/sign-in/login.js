6938671be19d5cfc86ce37725b0a7dd052c23b49
Bartek <bbogucki7@gmail.com>
2025-06-28 11:57:12 +0200

    implemenet gitignore

--- material-dashboard-backend/components/auth/sign-in/login.js
+++ material-dashboard-backend/components/auth/sign-in/login.js
@@ -52,12 +52,14 @@ router.post("/login", async (req, res) => {
 			res.cookie("accessToken", accessToken, {
 				maxAge: 15 * 60 * 1000,
 				secure: process.env.NODE_ENV === "production",
-				httpOnly: false,
+				httpOnly: true,
+				sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
 			});
 			res.cookie("refreshToken", refreshToken, {
 				maxAge: 30 * 24 * 60 * 60 * 1000,
 				secure: process.env.NODE_ENV === "production",
-				httpOnly: false,
+				httpOnly: true,
+				sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
 			});
 
 			return res.status(200).json({ message: "User logged successfull" });
